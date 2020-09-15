80a03e53af2f5cf888b345924e446b2dc75e9acb
RocioCentu <42979480+RocioCentu@users.noreply.github.com>
2020-09-15 14:04:38 -0300

    RocioCentu
    Terminado

---
+++ src/components/Producto.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+//redux
+import { useDispatch } from 'react-redux';
+import { eliminarProductoAction,obtenerProductoEditarAction} from '../actions/productoActions';
+
+const Producto =({producto}) => {
+    const { nombre, precio, id } = producto;
+  
+    const dispatch = useDispatch();
+    const history = useHistory(); // habilitar history para redirección
+
+    //confirmar eliminar Producto
+  const confirmarEliminarProd= id=>{
+      //preguntar al user
+      Swal.fire({
+        title: '¿Estas seguro?',
+        text: "Un producto que se elimina no se puede recuperar",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar!!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.value) {
+            // pasar al action
+            dispatch( eliminarProductoAction(id) );
+        }
+    });
+      
+
+  }
+
+  //redireccionar a la pagina de edicion con un prod seleccionado
+  const redireccionarEdicion=(producto)=>{
+      dispatch(obtenerProductoEditarAction(producto));
+       history.push(`/productos/editar/${producto.id}`);
+  }
+
+    return (    <tr>
+        <td>{nombre}</td>
+        <td><span className="font-weight-bold"> $ {precio} </span></td>
+        <td className="acciones">
+            <button 
+                type="button"
+                 onClick={()=> redireccionarEdicion(producto)}
+                className="btn btn-primary mr-2">
+                Editar
+            </button>
+            <button 
+                type="button"
+                className="btn btn-danger"
+                onClick={()=>confirmarEliminarProd(id)}
+            >Eliminar </button>
+        </td>
+    </tr>
+    )
+}
+ 
+export default Producto;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+//combona todos los states
+import { combineReducers } from 'redux';
+import productosReducer from './productosReducer';
+import alertaReducer from './alertaReducer';
+
+export default combineReducers({
+    productos: productosReducer,
+    alerta: alertaReducer
+});
